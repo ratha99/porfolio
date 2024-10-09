60673040f3abf750220915e4d916677e6532e717
Ratha <longratha.num@gmail.com>
2024-10-09 08:46:00 +0700

    .ENV

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer:{
-     proxy:'https://freetestapi.com/'
-    // proxy: process.env.VUE_APP_URL_DEMO_BACKEND
+    //proxy:'https://freetestapi.com/'
+     proxy: process.env.VUE_APP_URL_DEMO_BACKEND
   }
 })
