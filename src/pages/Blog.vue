f85e7ff2d5c30e9a6f47fbfacb713569fb0517b4
Ratha <longratha.num@gmail.com>
2024-10-11 09:15:18 +0700

    UpDATE api

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -37,11 +37,11 @@ export default {
         }
     },
     async created() {
-        var api = "https://dummyjson.com/c/f270-db0d-412f-a836";
+        var api = process.env.VUE_APP_BACKEND+"c/f270-db0d-412f-a836";
         if(this.$i18n.locale == "en"){
-            api = "https://dummyjson.com/c/f270-db0d-412f-a836"
+            api = process.env.VUE_APP_BACKEND+"c/f270-db0d-412f-a836"
         }else{
-            api ="https://dummyjson.com/c/0051-4a95-46ea-b2bd"
+            api =process.env.VUE_APP_BACKEND+"c/0051-4a95-46ea-b2bd"
         }
         const reponse = await axios.get(api);
         const blog = reponse.data
